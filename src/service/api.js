d9f46bf85af7550176ab972bcdc4852bbe5d449a
MoisesMed <br.moises@hotmail.com>
2024-09-25 01:42:30 -0300

    feat: New fetchs

--- src/service/api.js
+++ src/service/api.js
@@ -1,17 +1,28 @@
-export async function fetchProducts() {
-  const res = await fetch("https://fakestoreapi.com/products");
+export async function fetchProducts(page = 1, limit) {
+  const res = await fetch(
+    `https://fakestoreapi.in/api/products?page=${page}&limit=${limit}`
+  );
   if (!res.ok) {
     throw new Error("Failed to fetch products");
   }
   const products = await res.json();
-  return products;
+  return products.products;
+}
+
+export async function fetchCategories() {
+  const res = await fetch("https://fakestoreapi.in/api/products/category");
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+  const products = await res.json();
+  return products.categories;
 }
 
 export async function fetchSingleProduct(id) {
-  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const res = await fetch(`https://fakestoreapi.in/api/products/${id}`);
   if (!res.ok) {
     throw new Error("Failed to fetch product");
   }
   const product = await res.json();
-  return product;
+  return product.product;
 }
